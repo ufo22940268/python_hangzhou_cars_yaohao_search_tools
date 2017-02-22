1dd7b6bb5ed050a98e7f8dff40e812f95a0dc205
ylzhao <htzha0200744@gmail.com>
2017-02-22 21:55:35 +0800

    1. add parse all pdf  convert to  the json file 2. add get the pdf dir , filter all pdf, then call pdf_to_json to convert to json

--- parse_pdf.py
+++ parse_pdf.py
@@ -10,13 +10,13 @@ from cStringIO import StringIO
 import StringIO as SIO
 import json
 import re 
+import os
 
 
 
 class ParsePdf:
     def __init__(self,pdf):
         self.pdf = pdf
-        self.parse_pfd()
     
     def parse_pfd(self):
         fp = file(self.pdf, 'rb')
@@ -51,21 +51,29 @@ class ParsePdf:
                 lottery_dict['user_number'] = user_number.group(0)
             if user_name is not None:
                 lottery_dict['user_name'] = user_name.group(1)
-                print lottery_dict['user_name']
             lottery_items.append(lottery_dict)
         return lottery_items
     
     def pdf_to_txt(self):
+        
         # a = parse_data()
         # with open('./pdf/%s' %pdf_name, 'wb') as fd:
         #     fd.write(response)
         pass
-    
-    
-    def pdf_to_json(self):
-        pass
+        
+    def pdf_to_json(self, name):
+        data = self.parse_data()
+        with open('./json/%s' %name, 'wb') as f:
+            json.dump(data,f)
+        
+
 
     
 
 if __name__ == "__main__":
-    ParsePdf('./pdf/1409037121186.pdf').parse_data()
+    pdfs = filter( lambda f: not f.startswith('.'), os.listdir('./pdf'))
+    for i in pdfs:
+        pdf_path = './pdf/'+i
+        print pdf_path
+        json_name = i.replace('.pdf', '.json')    
+        ParsePdf(pdf_path).pdf_to_json(json_name)
